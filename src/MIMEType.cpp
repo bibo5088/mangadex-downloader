fc96ed5a21bfb2ce19d29c284e03f91f25bb0a76
Corentin Crébier <bibo5088@gmail.com>
2019-08-13 18:04:20 +0200

    Warnings

--- src/MIMEType.cpp
+++ src/MIMEType.cpp
@@ -1,7 +1,7 @@
 #include "MIMEType.h"
 
 MIMEType::MIMEType(std::string_view mime_type) {
-    int pos = mime_type.find_first_of('/');
+    std::size_t pos = mime_type.find_first_of('/');
     type = mime_type.substr(0, pos);
     name = mime_type.substr(pos + 1);
 }
